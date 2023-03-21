ca6e3349d1d06ffd004589006262e6d2ec1033d7
Marta Klakovych <101287791+martysiaklak@users.noreply.github.com>
2023-03-21 19:14:38 +0000

    created functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <chrono>
+#include <string>
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+using namespace std;
+using namespace std::chrono;
 
+class Plant 
+{
+private:
+    string name;
+    long long growth_time;
+    system_clock::time_point start_time;
+    int quantity;
+    double price;   
+    string colour;    
+public:
+    Plant(string name, long long growth_time) 
+    {
+        this->name = name;
+        this->growth_time = growth_time;
+        this->start_time = system_clock::now();
+    }
+
+    string getName() //getting name
+    {
+        return name;
+    }
+
+    long long getGrowthTime() //getting time that is needed to grow a plant
+    {
+        return growth_time;
+    }
+
+    long long getSecondsGrown()   //calculates the number of seconds that have elapsed since the plant was created
+  {
+        auto elapsed_time = duration_cast<seconds>(system_clock::now() - start_time);
+        return elapsed_time.count();
+  }
+};
